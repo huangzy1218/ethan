17697be0c9e420784a836c6cc3f9a7c04171bd6b
zhengyang.huang <2691386627@qq.com>
2024-06-05 18:37:24 +0800

    chore: Netty server updates

--- ethan-remoting/src/main/java/com/ethan/remoting/tansport/netty/NettyServer.java
+++ ethan-remoting/src/main/java/com/ethan/remoting/tansport/netty/NettyServer.java
@@ -1,6 +1,7 @@
 package com.ethan.remoting.tansport.netty;
 
 import com.ethan.common.URL;
+import com.ethan.common.util.CollectionUtils;
 import com.ethan.common.util.NamedThreadFactory;
 import com.ethan.remoting.Channel;
 import com.ethan.remoting.RemotingException;
@@ -8,9 +9,11 @@ import com.ethan.remoting.RemotingServer;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -48,19 +51,59 @@ public class NettyServer implements RemotingServer {
                 true));
         ExecutorService worker =
                 Executors.newCachedThreadPool(new NamedThreadFactory(EVENT_LOOP_WORKER_POOL_NAME, true));
-        // todo
-        //        ChannelFactory channelFactory = new NioServerSocketChannelFactory(
-//                boss, worker, getUrl().getPositiveParameter(IO_THREADS_KEY, DEFAULT_IO_THREADS));
+        ChannelFactory channelFactory = new NioServerSocketChannelFactory(boss, worker);
+        bootstrap = new ServerBootstrap(channelFactory);
+        bootstrap.setOption("child.tcpNoDelay", true);
+        channel = bootstrap.bind();
     }
 
     @Override
     public void doClose() {
-
+        try {
+            if (channel != null) {
+                // Unbind
+                channel.close();
+            }
+        } catch (Throwable e) {
+            log.warn("Transport closed failed: {}", e.getMessage(), e);
+        }
+        try {
+            Collection<Channel> channels = getChannels();
+            if (CollectionUtils.isNotEmpty(channels)) {
+                for (Channel channel : channels) {
+                    try {
+                        channel.close();
+                    } catch (Throwable e) {
+                        log.warn("Transport closed failed: {}", e.getMessage(), e);
+                    }
+                }
+            }
+        } catch (Throwable e) {
+            log.warn("Transport closed failed: {}", e.getMessage(), e);
+        }
+        try {
+            if (bootstrap != null) {
+                // Release external resource.
+                bootstrap.releaseExternalResources();
+            }
+        } catch (Throwable e) {
+            log.warn("Transport closed failed: {}", e.getMessage(), e);
+        }
+        try {
+            if (channels != null) {
+                channels.clear();
+            }
+        } catch (Throwable e) {
+            log.warn("Transport closed failed: {}", e.getMessage(), e);
+        }
     }
 
     @Override
     public Collection<Channel> getChannels() {
-        return List.of();
+        Collection<Channel> chs = new ArrayList<>(this.channels.size());
+        // Pick channels from NettyServerHandler (needless to check connectivity)
+        chs.addAll(this.channels.values());
+        return chs;
     }
 
 }
