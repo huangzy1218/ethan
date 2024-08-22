e4221441d5d9836ce42994da9002d72ccf71473f
huangzhengyang <uanghengyang@xiaomi.com>
2024-08-22 22:09:09 +0800

    refactor: Remove useless code

--- ethan-cluster/src/main/java/com/ethan/rpc/cluster/loadbalance/RoundRobinLoadBalancer.java
+++ ethan-cluster/src/main/java/com/ethan/rpc/cluster/loadbalance/RoundRobinLoadBalancer.java
@@ -5,52 +5,13 @@ import com.ethan.rpc.Invocation;
 import com.ethan.rpc.Invoker;
 
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Round robin loadbalancer.
  *
  * @author Huang Z.Y.
- * <p>
- * 当前轮询的下标
- * <p>
- * 当前轮询的下标
- * <p>
- * 当前轮询的下标
- * <p>
- * 当前轮询的下标
- * <p>
- * 当前轮询的下标
- * <p>
- * 当前轮询的下标
- * <p>
- * 当前轮询的下标
- * <p>
- * 当前轮询的下标
- * <p>
- * 当前轮询的下标
- * <p>
- * 当前轮询的下标
  */
-/**
- * 当前轮询的下标
- */
-
-@Override
-public ServiceMetaInfo select(Map<String, Object> requestParams, List<ServiceMetaInfo> serviceMetaInfoList) {
-    if (serviceMetaInfoList.isEmpty()) {
-        return null;
-    }
-    // 只有一个服务，无需轮询
-    int size = serviceMetaInfoList.size();
-    if (size == 1) {
-        return serviceMetaInfoList.get(0);
-    }
-    // 取模算法轮询
-    int index = currentIndex.getAndIncrement() % size;
-    return serviceMetaInfoList.get(index);
-}
 
 public class RoundRobinLoadBalancer extends AbstractLoadBalance {
 
@@ -58,8 +19,7 @@ public class RoundRobinLoadBalancer extends AbstractLoadBalance {
      * The index of the current poll.
      */
     private final AtomicInteger currentIndex = new AtomicInteger(0);
-
-
+    
     @Override
     protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) {
         if (invokers.isEmpty()) {
@@ -70,7 +30,7 @@ public class RoundRobinLoadBalancer extends AbstractLoadBalance {
         if (size == 1) {
             return invokers.get(0);
         }
-        // 取模算法轮询
+        // Modulo algorithm polling
         int index = currentIndex.getAndIncrement() % size;
         return invokers.get(index);
     }
